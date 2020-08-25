ff73332d674e5c089691f2e41becba90d754ebd7
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2020-08-24 22:42:43 -0300

    [Scripts e divisorias] Criados scripts em package.json para gerar os arquivos JavaScript e inseridas divisorias na exibicao de cada exercicio.

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,21 @@ import fatorial from "./fatorial";
 import multiplos from "./multiplos";
 
 // Exercício 1 - Eleições
+console.log("EXERCÍCIO 1 - ELEIÇÕES");
 eleicao();
+console.log("###########################################\n\n\n\n");
 
 // Exercício 2 - Bubble Sort
+console.log("EXERCÍCIO 2 - BUBBLE SORT");
 bubbleSort();
+console.log("###########################################\n\n\n\n");
 
 // Exercício 3 - Fatotial
+console.log("EXERCÍCIO 3 - FATORIAL");
 fatorial(5);
+console.log("###########################################\n\n\n\n");
 
 // Exercício 4 - Múltiplos de 3 e 5
+console.log("EXERCÍCIO 2 - MÚLTIPLOS");
 multiplos(10);
+console.log("###########################################");
